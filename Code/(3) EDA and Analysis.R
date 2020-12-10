c0ff19d9d8638c0bd0a76c2860ba9030f09b4836
Suppapat Kevin Korsurat <skorsu@umich.edu>
2020-12-10 17:00:19 -0500

    update plot and balance table

--- Code/(3) EDA and Analysis.R
+++ Code/(3) EDA and Analysis.R
@@ -10,7 +10,7 @@
 ## Updated: November 29, 2020
 
 # libraries: ------------------------------------------------------------------
-library(dplyr)
+library(tidyverse)
 library(data.table)
 library(ggplot2)
 library(gridExtra)
@@ -29,25 +29,59 @@ if(! dir.exists(paste0(path, "Plots"))){
 # Data: -----------------------------------------------------------------------
 data <- suppressWarnings(fread(paste0(main_url, repo, data_loc)))
 
-# Function: -------------------------------------------------------------------
-## Function for EDA (1 Variable)
-### I have adapted the method provided in https://stackoverflow.com/a/14577878
-### in order to use the function input to be the labels of X axis and title of
-### the plot.
+## Convert the type of variables into factor variables
+data <- data.frame(data[,1], sapply(data[,-1], factor))
 
-eda_1var <- function(vari){
+# Function: -------------------------------------------------------------------
+
+## Function: Number with comma.
+### Input: Number that you wish to convert into xx,xxx form
+### Output: Number in xx,xxx form
+
+comma_th <- function(number){
+  if(number < 1000){
+    return(paste0(number))
+  }
+  else {
+    after <- number %% 1000
+    if(after <= 9){
+      after <- paste0("00", after)
+    } else if (after <= 99) {
+      after <- paste0("0", after)
+    }
+    
+    before <- floor(number/1000)
+    return(paste0(before, ",", after))
+  }
+}
+
+## Function: Bar chart for one variable.
+### Input: (Required) - Variable name 
+###        (Optional) - Color of the bar (default = "salmon")
+###        (Optional) - Size of the number and percent (default = 3.5)
+### Output: Bar chart
+
+#### I have adapted the method provided in https://stackoverflow.com/a/14577878
+#### in order to use the function input to be the labels of X axis and title of
+#### the plot.
+
+eda_1var <- function(vari, col_bar = "salmon", num_size = 3.5){
   eda_tab <- data %>% 
     group_by({{vari}}) %>% 
     summarise(n = n()) %>% 
     mutate(Percent = 100 * n/sum(n))
   
+  eda_tab$n <- unlist(lapply(eda_tab$n, comma_th))
+  
   eda_plot <- ggplot(eda_tab, 
                      aes_string(x = names(eda_tab)[1], 
                                 y = names(eda_tab)[3])) +
-    geom_bar(stat = "identity", width = 0.3, fill = "#74F185") +
+    geom_bar(stat = "identity", width = 0.3, fill = col_bar) +
     theme_bw() +
+    geom_text(aes(label = paste0("(", n, ")")), 
+              vjust = 1.5, color = "black", size = num_size) +
     geom_text(aes(label = paste0(round(Percent,2), "%")), 
-              vjust = 0.05, color = "black", size = 3) +
+              vjust = -0.25, color = "black", size = num_size) +
     scale_y_continuous(name = "Percent", limits = c(0, 100))
   
   if(! deparse(substitute(vari)) %in% c("Gender", "Age_Group")){
@@ -62,6 +96,51 @@ eda_1var <- function(vari){
     eda_plot <- eda_plot +
       labs(x = deparse(substitute(vari)), title = (deparse(substitute(vari)))) 
   }
+}
+
+## Function: Balance Table
+### Input: Variable name
+### Output: (Number of class for that variable) x 5 tables, providing the 
+###         information on the percentage for each class, along with the 
+###         percentage by each class of hypertension status.
+
+balance_table <- function(vari, data_used = data){
+  
+  ## Relevel the factor variables
+  data_used$Hypertension <- relevel(data_used$Hypertension, 2)
+  data_used$Diabetes <- relevel(data_used$Diabetes, 2)
+  data_used$Obesity <- relevel(data_used$Obesity, 2)
+  
+  ## Initial the table
+  bal_tab <- data_used %>%
+    group_by({{vari}}, Hypertension) %>%
+    summarise(n = n()) %>%
+    ungroup() %>%
+    spread(Hypertension, n)
+  
+  ## Column for Variable Name
+  if(colnames(bal_tab)[1] != "Age_Group"){
+    v_name <- c(colnames(bal_tab)[1], rep("", dim(bal_tab)[1] - 1))
+  } else {
+    v_name <- c("Age Group", rep("", dim(bal_tab)[1] - 1))
+  }
+
+  ## Create the Summary table
+  colnames(bal_tab)[1] <- "Class"
+  bal_tab$row_sum <- apply(bal_tab[,-1], 1, sum)
+  bal_tab %>% 
+    mutate(HY = paste0(comma_th(bal_tab$Yes), " (", 
+                       round(100*Yes/row_sum, 2), "%)"),
+           HN = paste0(comma_th(bal_tab$No), " (", 
+                       round(100*No/row_sum, 2), "%)"),
+           by_var = paste0(comma_th(bal_tab$row_sum), " (", 
+                           round(100*row_sum/sum(row_sum), 2), "%)"),
+           Variable = v_name) %>%
+    select(Variable, Class,
+           "By Class (%Class)" = by_var,
+           "Hypertension: Yes" = HY, 
+           "Hypertension: No" = HN) %>%
+    return()
 }
 
 ## Function for EDA (Hypertension x Diabetes x Group)
@@ -101,16 +180,19 @@ eda_cross <- function(vari){
     labs(title = title_plot)
 }
 
-# Data Structure: -------------------------------------------------------------
-dim(data)
-str(data)
+# Exploratory Data Analysis ---------------------------------------------------
 
-# EDA (1 Variables): ----------------------------------------------------------
-## Response Variable
-ggsave(paste0(path, "Plots/Hypertension.png"), eda_1var(Hypertension))
+## Hypertension
+print(eda_1var(Hypertension, "aquamarine2"))
 
-## Predictors
-ggsave(paste0(path, "Plots/Gender.png"), eda_1var(Gender))
+ggsave(paste0(path, "Plots/Hypertension.png"), 
+       eda_1var(Hypertension, "aquamarine2", 4.5))
+
+## Summary Table
+bal_tab_all <- rbind(suppressWarnings(balance_table(Diabetes)),
+                     suppressWarnings(balance_table(Obesity)),
+                     suppressWarnings(balance_table(Gender)),
+                     suppressWarnings(balance_table(Age_Group)))
 
 ## Groups
 ggsave(paste0(path, "Plots/Group.png"),
@@ -126,26 +208,31 @@ hd_table <- data %>%
   group_by(Diabetes) %>%
   mutate(Percent = 100 * n/sum(n))
 
+hd_table$n <- unlist(lapply(hd_table$n, comma_th))
+
 hd_table$Diabetes <- factor(hd_table$Diabetes)
 hd_table$Diabetes <- relevel(hd_table$Diabetes, 2)
 
 hypertension_diabetes <- ggplot(hd_table, aes(x = Hypertension, y = Percent)) +
-  geom_bar(stat = "identity", width = 0.3, fill = "salmon") +
+  geom_bar(stat = "identity", width = 0.4, fill = "salmon") +
   facet_grid(. ~ Diabetes, labeller = label_both) +
   theme_bw() +
-  geom_text(aes(label = paste0("(",n,")")), 
-            vjust = 1.5, color = "black", size = 3) +
+  geom_text(aes(label = paste0("(", n, ")")), 
+            vjust = 1.5, color = "black", size = 3.5) +
   geom_text(aes(label = paste0(round(Percent,2), "%")), 
-            vjust = -0.25, color = "black", size = 3) +
+            vjust = -0.25, color = "black", size = 3.5) +
   scale_y_continuous(name = "Percent", limits = c(0, 100)) +
   scale_x_discrete(limits=c("Yes", "No")) +
   labs(title = "Hypertension status classified by Diabetes")
 
-# hypertension_diabetes
+hypertension_diabetes
 
 ggsave(paste0(path, "Plots/hypertension_diabetes.png"), hypertension_diabetes)
 
 # EDA (Hypertension x Diabetes x Group): --------------------------------------
+
+data
+
 c_p <- grid.arrange(eda_cross(Gender), 
                     eda_cross(Age_Group),
                     eda_cross(Obesity),
--- Plots/README.md
+++
@@ -1,2 +0,0 @@
-# Plots
-This folder contains the plots, resulted from (3) (3) EDA and Analysis.R. These plots are used in the final report.
